b4a360fbdf37c5f59ff3a0b44ba211ee86a0fb4b
FelipeHeide <felipe.heide.work@gmail.com>
2024-06-14 11:29:01 -0300

    COMPLETE

---
+++ functions.py
@@ -0,0 +1,94 @@
+import requests
+from groq import Groq
+from googlesearch import search
+from heyoo import WhatsApp
+from deep_translator import GoogleTranslator
+
+
+
+
+def sendMessage(message, type):
+    token = 'EAACcosEiDGcBOZBZB0NYdX68j7TgXATeDNZAlKRe7hiD9awV0ZCIyjRl2eaAkAZBv3NxZBAfRlnLRn9hgW3HsZBREHv1QIZBAA4MomJ40IeDdFnGdEPwcvjv2F01rMSZCbFBYvF9Rct5ZCeqsZBzXhZBB89kycr0uXIIRS63hCrGOqRwoZBvGcq7tZCOnW6WkbAKrcn4z5MyYayZBYZBlBIo3UK2xZAnEWVAB6ApU'
+    idNumeroTeléfono = '108450905597878'
+    mensajeWa = WhatsApp(token, idNumeroTeléfono)
+    if type == "text":
+      mensajeWa.send_message(message, "54111524612307")
+    elif type == "image":
+      mensajeWa.send_image(message, "54111524612307")
+    elif type == "audio": 
+       mensajeWa.send_audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", "54111524612307")
+
+
+client = Groq(api_key="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")
+
+def grok_functionality(message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",    
+                "content": "You will receive a message from a WhatsApp user. Identify the user's need and respond with one of the specific tasks listed below in capital letters, followed by the relevant prompt. Do not translate any words or phrases; keep them in their original language. /n- If the user wants a word or phrase translated, respond with 'TRANSLATION_LANGUAGE: <Original Word/Phrase>'./n- If the user wants to search for an image online, and uses words like search image, find image, respond with 'IMAGE_SEARCH: <Image Search Prompt>'./n- If the user wants a definition and uses words like 'define' or 'definition', respond with 'DEFINITION: <Word>'./n- If the user requests a list of possible functions, respond with 'FUNCTIONS'./n- If the user wants a chat completion (e.g., searching for information, or chatting, or needs a text, asking a question for specific information), respond with 'CHAT_COMPLETION: <Exact User Prompt Chat>'./n- If the user wants to listen to a song, respond with 'SONG: <Song Title>'./n- If the user wants to search for information on Google (e.g., searching for links, sites), inthe case that user uses words like search, links, or specific site respond with 'GOOGLE_SEARCH: <Search Prompt For User Necessities>'./n/nRespond only with the task and prompt, nothing else. For example, if the user wants to translate 'Cat' to French, respond with 'TRANSLATION_FR: Cat'. If the user wants an image search, respond with 'IMAGE_Search: <Image Search Prompt>', and do not put in the image prompt the Exact User Prompt Chat, you should do the prompt for that specific image search. Do not mistake if the user wnats a CHAT_COMPLETION or GOOGLE_SEARCH, if user exact prompt contains words likes search, links or online then he wants a GOOGLE_SEARCH, if he just wants to talk or have a specific information then it is CHAT_COMPLETION. When writting the Search Prompt For User Necessities do not write the specific Exact User Prompt Chat, write only what he wants to search./nExact User Prompt Chat:\n" + message,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    return chat_completion.choices[0].message.content
+
+def grok_chat(message):
+    
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",    
+                "content": message,
+                }
+        ],
+        model="llama3-8b-8192",
+    )
+    return chat_completion.choices[0].message.content
+
+def google_search(prompt):
+    search_results = search(prompt, lang="en", advanced=True, num_results=5)
+    message = "Google Search Results for: '{}'\n\n".format(prompt)
+    for result in search_results:
+        message += "Title: {}\n".format(result.title)
+        message += "URL: {}\n".format(result.url)
+        message += "Description: {}\n\n".format(result.description)
+
+    return message
+
+def get_definition(palabra):
+  url = "https://api.wordnik.com/v4/word.json/"
+  endpoint = f"{url}{palabra}/definitions"
+  params = {
+    "limit": 1,
+    "includeRelated": "false",
+    "useCanonical": "false",
+    "api_key": "ir2gi4h02gbswj7oxdv2qr81srt7y16fo1cx0ajyfo9htx7fy"
+  }
+
+  response = requests.get(endpoint, params=params)
+
+  definition = response.json()[0]["text"]
+
+  return palabra + ": "+definition
+
+def img(mens):
+  url = 'https://google.serper.dev/images'
+  headers = {
+    'X-API-KEY': 'e27c034275fb2b03c97c6f6ff1685be013cbc531',
+    'Content-Type': 'application/json'
+  }
+  data = {'q': mens, 'hl': 'es'}
+
+  response = requests.post(url, headers=headers, json=data)
+  result = response.json()
+  images = result['images']
+  first_image_url = images[0]['imageUrl']
+  return first_image_url
+
+def trans(mensaje, lang):
+    lengua = lang.lower()
+    translated = GoogleTranslator(source='auto', target=lengua).translate(mensaje)
+    textoMensaje = f"{mensaje}\n\n{lang}: {translated}"
+    return textoMensaje
+
---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+import functions
+
+app = Flask(__name__)
+
+
+
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    if request.method == "GET":
+        if request.args.get('hub.verify_token') == "FelipeWaBot":
+            return request.args.get('hub.challenge')
+        else:
+            return "Error de autentificacion."
+
+    if request.method == "POST":
+        data = request.get_json()
+        if data:
+            try:
+                tipo_msg = data['entry'][0]['changes'][0]['value']['messages'][0]['type']
+                data['entry'][0]['changes'][0]['value']['messages'][0]['type']
+
+                if tipo_msg == "text":
+                    mensaje = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+                    func_response = functions.grok_functionality(mensaje)
+                    functions.sendMessage(func_response, "text")
+                    if func_response.startswith('TRANSLATION_'):
+                        parts = func_response.split(': ')
+                        language = parts[0].split('_')[1]
+                        text = parts[1]
+                        functions.sendMessage(functions.trans(text, language), "text")
+                    elif func_response.startswith('IMAGE_SEARCH'):
+                        query = func_response.split('IMAGE_SEARCH: ')[1]
+                        functions.sendMessage(functions.img(query), "image")
+                    elif func_response.startswith('DEFINITION'):
+                        word = func_response.split('DEFINITION: ')[1]
+                        functions.sendMessage(functions.get_definition(word), "text")
+                    elif func_response.startswith('CHAT_COMPLETION'):
+                        chat_prompt = func_response.split('CHAT_COMPLETION: ')[1]
+                        functions.sendMessage(functions.grok_chat(chat_prompt), "text")
+                    elif func_response.startswith('GOOGLE_SEARCH'):
+                        search_prompt = func_response.split('GOOGLE_SEARCH: ')[1]
+                        functions.sendMessage(functions.google_search(search_prompt), "text")
+                    elif func_response.startswith('FUNCTIONS'):
+                        functions.sendMessage("Our Functions are the following:\n\nTRANSLATION_LANGUAGE: Translate words or text in any language you want.\nIMAGE_SEARCH: Search for any image you want.\nDEFINITION: Get the definition of any word.\nCHAT_COMPLETION: Continue a conversation on any topic.\nGOOGLE_SEARCH: Search Google for any topic and get relevant links.\nSONG: Play any song you want.\nFUNCTIONS: Get a list of all available functions.", "text")
+                    elif func_response.startswith('SONG'):
+                        song_title = func_response.split('SONG: ')[1]
+                        functions.sendMessage(song_title, "audio")
+                   
+
+
+                        
+            except KeyError as e:
+                print(f"KeyError: {e}")
+        
+        return "Webhook received", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
